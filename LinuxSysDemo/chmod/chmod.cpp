b7ec6123a832dbe957b0935e7831a4f97a7d6c2c
fjkfjk <18629337250@163.com>
2017-02-12 04:29:34 -0800

    add a implement of chmod command

---
+++ LinuxSysDemo/chmod/chmod.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char** argv)
+{
+  int mode;
+  int mode_u;    //users
+  int mode_g;    //group
+  int mode_o;    //other user
+
+  char* file_path;
+
+  if(argc < 3)
+  {
+    printf("%s <mode num> <target file>\n",argv[0]);
+    exit(0);
+  }
+
+  mode = atoi(argv[1]);
+  if(mode > 777 || mode < 0)
+  {
+    printf("mode num error!\n");
+    exit(0);
+  }
+
+  mode_u = mode / 100;
+  mode_g = (mode - mode_u * 100) /10;
+  mode_o = mode - mode_u * 100 - mode_g * 10;
+  mode = mode_u * 8 * 8 + mode_g * 8 + mode_o;
+
+  file_path = argv[2];
+
+  if(chmod(file_path,mode) == -1)
+  {
+    perror("chmod error");
+    exit(1);
+  }
+
+  return 0;
+}
